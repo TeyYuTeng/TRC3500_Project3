08eebc768bb420022c110563d8b6a33016f9d4b7
TeyYuTeng <ytey0004@student.monash.edu>
2025-05-29 10:57:50 +0800

    plot_csv.py update

--- plot_csv.py
+++ plot_csv.py
@@ -1,8 +1,36 @@
 import matplotlib.pyplot as plt
+import time
 import csv
+import os
+
+def moving_average(signal, window_size=5):
+    """Applies a simple moving average filter to the signal."""
+    return [sum(signal[i:i+window_size])/window_size if i+window_size <= len(signal) else signal[i]
+            for i in range(len(signal))]
+
+def threshold_peak_detector(signal, threshold):
+    """
+    Detects peaks in a signal based on threshold crossing:
+    A peak is counted when the signal rises above the threshold and
+    then falls below it.
+    """
+    peaks = []
+    above = False
+
+    for i in range(1, len(signal)):
+        if not above and signal[i] > threshold:
+            above = True
+        elif above and signal[i] <= threshold:
+            peaks.append(i)
+            above = False
+
+    return peaks
 
 # === Settings ===
-csv_file = 'trial5.csv'
+csv_file = 'test.csv'
+time_window = 30
+sample_interval = 0.1  # seconds per sample
+num_samples = int(time_window/sample_interval)
 
 # === Data containers ===
 time_vals = []
@@ -21,6 +49,36 @@ with open(csv_file, newline='') as file:
         time_vals.append(float(row[0]))
         adc1_vals.append(int(row[1]))
         adc2_vals.append(int(row[2]))
+
+    # Apply moving average filter
+    filtered_adc1 = moving_average(adc1_vals, window_size=5)
+    filtered_adc2 = moving_average(adc2_vals, window_size=5)
+
+    # Thermistor
+    threshold1 = (max(filtered_adc1) + min(filtered_adc1))/2
+    peaks1 = threshold_peak_detector(filtered_adc1, threshold1)
+    est_rate_1 = (len(peaks1) * 60) / (num_samples * sample_interval)
+
+    print("=========== Thermistor Sensor ==========")
+    print(f"Estimated rate: {est_rate_1:.1f} bpm")
+    print(f"Peaks detected: {len(peaks1)}\n")
+
+    # Conductive band Sensor
+    threshold2 = (max(filtered_adc2) + min(filtered_adc2))/2
+    peaks2 = threshold_peak_detector(filtered_adc2, threshold2)
+    est_rate_2 = (len(peaks2) * 60) / (num_samples * sample_interval)
+
+    print("=========== Conductive band Sensor ==========")
+    print(f"Estimated rate: {est_rate_2:.1f} bpm")
+    print(f"Peaks detected: {len(peaks2)}\n")
+
+    weight1 = 0.9  # confidence in thermistor
+    weight2 = 1 - weight1  # confidence in conductive band
+
+    fused_bpm = (weight1 * est_rate_1) + (weight2 * est_rate_2)
+
+    print("=========== Final BPM ==========")
+    print(f"Estimated rate (fused): {round(fused_bpm)} bpm")
 
 # === Plotting ===
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 5), sharex=True)
